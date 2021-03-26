391b9afc2d496bea9070f0007dc60006a744d9de
LimeMoon <LimeMoon@kakao.com>
2021-03-26 15:38:39 +0900

    210326_000

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,330 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_kakao_login/flutter_kakao_login.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final FlutterKakaoLogin kakaoSignIn = FlutterKakaoLogin();
+
+  String _loginMessage = 'Current Not Logined :(';
+  String _accessToken = '';
+  String _refreshToken = '';
+  String _accountInfo = '';
+  bool _isLogined = false;
+
+  final List<Map<String, String>> _listItems = [
+    {'key': 'login', 'title': 'Login', 'subtitle': ''},
+    {'key': 'logout', 'title': 'Logout', 'subtitle': ''},
+    {'key': 'unlink', 'title': 'Unlink', 'subtitle': ''},
+    {'key': 'account', 'title': 'Get AccountInfo', 'subtitle': ''},
+    {'key': 'accessToken', 'title': 'Get AccessToken', 'subtitle': ''},
+    {'key': 'refreshToken', 'title': 'Get RefreshToken', 'subtitle': ''}
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  void load() async {
+    // Kakao SDK Init (Set NATIVE_APP_KEY)
+    await kakaoSignIn.init('847e2047fa1b48778c169fed0f685133');
+
+    // For Android
+    final hashKey = await kakaoSignIn.hashKey;
+    print('hashKey: $hashKey');
+  }
+
+  Future<Null> _login() async {
+    try {
+      final result = await kakaoSignIn.logIn();
+      _processLoginResult(result);
+    } on PlatformException catch (e) {
+      _updateLoginMessage('${e.code} ${e.message}');
+    }
+  }
+
+  Future<Null> _logOut() async {
+    try {
+      final result = await kakaoSignIn.logOut();
+      _processLoginResult(result);
+      _processAccountResult(null);
+    } on PlatformException catch (e) {
+      _updateLoginMessage('${e.code} ${e.message}');
+    }
+  }
+
+  Future<Null> _unlink() async {
+    try {
+      final result = await kakaoSignIn.unlink();
+      _processLoginResult(result);
+    } on PlatformException catch (e) {
+      _updateLoginMessage('${e.code} ${e.message}');
+    }
+  }
+
+  Future<Null> _getAccountInfo() async {
+    try {
+      final result = await kakaoSignIn.getUserMe();
+      final account = result.account;
+      _processAccountResult(account);
+    } on PlatformException catch (e) {
+      _updateLoginMessage('${e.code} ${e.message}');
+    }
+  }
+
+  Future<Null> _getAccessToken() async {
+    final token = await kakaoSignIn.currentToken;
+    final accessToken = token?.accessToken;
+    if (accessToken != null) {
+      _updateAccessToken('AccessToken\n' + accessToken);
+    } else {
+      _updateAccessToken('');
+    }
+  }
+
+  Future<Null> _getRefreshToken() async {
+    final token = await kakaoSignIn.currentToken;
+    final refreshToken = token?.refreshToken;
+    if (refreshToken != null) {
+      _updateRefreshToken('RefreshToken\n' + refreshToken);
+    } else {
+      _updateRefreshToken('');
+    }
+  }
+
+  void _updateLoginMessage(String message) {
+    setState(() {
+      _loginMessage = message;
+    });
+  }
+
+  void _updateStateLogin(bool logined) {
+    setState(() {
+      _isLogined = logined;
+    });
+    if (!logined) {
+      _updateAccessToken('');
+      _updateRefreshToken('');
+      _updateAccountMessage('');
+    }
+  }
+
+  void _updateAccessToken(String accessToken) {
+    setState(() {
+      _accessToken = accessToken;
+    });
+  }
+
+  void _updateRefreshToken(String refreshToken) {
+    setState(() {
+      _refreshToken = refreshToken;
+    });
+  }
+
+  void _updateAccountMessage(String message) {
+    setState(() {
+      _accountInfo = message;
+    });
+  }
+
+  void _processLoginResult(KakaoLoginResult result) {
+    switch (result.status) {
+      case KakaoLoginStatus.loggedIn:
+        _updateLoginMessage('LoggedIn by the user.');
+        _updateStateLogin(true);
+        break;
+      case KakaoLoginStatus.loggedOut:
+        _updateLoginMessage('LoggedOut by the user.');
+        _updateStateLogin(false);
+        break;
+      case KakaoLoginStatus.unlinked:
+        _updateLoginMessage('Unlinked by the user.');
+        _updateStateLogin(false);
+        break;
+    }
+  }
+
+  void _processAccountResult(KakaoAccountResult? account) {
+    if (account == null) {
+      _updateAccountMessage('');
+    } else {
+      final userID = (account.userID == null) ? 'None' : account.userID;
+      final userEmail =
+          (account.userEmail == null) ? 'None' : account.userEmail;
+      final userPhoneNumber =
+          (account.userPhoneNumber == null) ? 'None' : account.userPhoneNumber;
+      final userDisplayID =
+          (account.userDisplayID == null) ? 'None' : account.userDisplayID;
+      final userNickname =
+          (account.userNickname == null) ? 'None' : account.userNickname;
+      final userGender =
+          (account.userGender == null) ? 'None' : account.userGender;
+      final userAgeRange =
+          (account.userAgeRange == null) ? 'None' : account.userAgeRange;
+      final userBirthyear =
+          (account.userBirthyear == null) ? 'None' : account.userBirthyear;
+      final userBirthday =
+          (account.userBirthday == null) ? 'None' : account.userBirthday;
+      final userProfileImagePath = (account.userProfileImagePath == null)
+          ? 'None'
+          : account.userProfileImagePath;
+      final userThumbnailImagePath = (account.userThumbnailImagePath == null)
+          ? 'None'
+          : account.userThumbnailImagePath;
+
+      _updateAccountMessage('- ID is $userID\n'
+          '- Email is $userEmail\n'
+          '- PhoneNumber is $userPhoneNumber\n'
+          '- DisplayID is $userDisplayID\n'
+          '- Nickname is $userNickname\n'
+          '- Gender is $userGender\n'
+          '- Age is $userAgeRange\n'
+          '- Birthyear is $userBirthyear\n'
+          '- Birthday is $userBirthday\n'
+          '- ProfileImagePath is $userProfileImagePath\n'
+          '- ThumbnailImagePath is $userThumbnailImagePath');
+    }
+  }
+
+  void _showAlert(BuildContext context, String value) {
+    if (value.isEmpty) return;
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(value, style: TextStyle(fontWeight: FontWeight.bold)),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: Text('OK'),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Kakao Login Plugin app'),
+        ),
+        body: SafeArea(
+          child: ListView.builder(
+            itemCount: _listItems.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return KakaoInfo(
+                  loginMessage: _loginMessage,
+                  accessToken: _accessToken,
+                  refreshToken: _refreshToken,
+                  accountInfo: _accountInfo,
+                );
+              }
+
+              final actionIndex = index - 1;
+              final title = _listItems[actionIndex]['title'] ?? '';
+              final subtitle = _listItems[actionIndex]['subtitle'] ?? '';
+              final key = _listItems[actionIndex]['key'];
+
+              return ListTile(
+                title: Text(title),
+                subtitle: Text(subtitle),
+                onTap: () {
+                  switch (key) {
+                    case 'login':
+                      if (!_isLogined) {
+                        _login();
+                      }
+                      break;
+                    case 'logout':
+                      if (_isLogined) {
+                        _logOut();
+                      }
+                      break;
+                    case 'unlink':
+                      if (_isLogined) {
+                        _unlink();
+                      }
+                      break;
+                    case 'account':
+                      if (!_isLogined) {
+                        _showAlert(context, 'Login is required.');
+                      } else {
+                        _getAccountInfo();
+                      }
+                      break;
+                    case 'accessToken':
+                      if (!_isLogined) {
+                        _showAlert(context, 'Login is required.');
+                      } else {
+                        _getAccessToken();
+                      }
+                      break;
+                    case 'refreshToken':
+                      if (!_isLogined) {
+                        _showAlert(context, 'Login is required.');
+                      } else {
+                        _getRefreshToken();
+                      }
+                      break;
+                  }
+                },
+              );
+            },
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+/////////////////////////////////////////////////////////////////////////////
+class KakaoInfo extends StatelessWidget {
+  final String loginMessage;
+  final String accessToken;
+  final String refreshToken;
+  final String accountInfo;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  KakaoInfo({
+    this.loginMessage = '',
+    this.accessToken = '',
+    this.refreshToken = '',
+    this.accountInfo = '',
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      padding: const EdgeInsets.fromLTRB(18, 25, 18, 25),
+      color: Colors.grey[300],
+      child: Column(
+        children: [
+          Text(loginMessage),
+          accountInfo != '' ? SizedBox(height: 25) : Container(),
+          accountInfo != '' ? Text(accountInfo) : Container(),
+          accessToken != '' ? SizedBox(height: 10) : Container(),
+          accessToken != '' ? Text(accessToken) : Container(),
+          refreshToken != '' ? SizedBox(height: 10) : Container(),
+          refreshToken != '' ? Text(refreshToken) : Container(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
